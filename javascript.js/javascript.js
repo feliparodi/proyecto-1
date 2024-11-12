b545907bf92b459017f8b905287fd89accc9b149
feliparodi <feliparodi2007@gmail.com>
2024-11-12 18:30:47 -0300

    Add files via upload

---
+++ javascript.js/javascript.js
@@ -0,0 +1,8 @@
+alert("esto va a ser epico papus")
+function cambiarfondo1()
+ { document.body.style.backgroundImage = "none";
+   document.body.style.backgroundColor = "black";
+document.body.style.color = "white"}
+
+function datos1()
+{ alert( "soy felipe parodi, tengo 17 años y me gusta jugar al hockey y al futbol" )}
